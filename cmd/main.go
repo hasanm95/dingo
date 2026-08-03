09072015f8f157922ee4bbcd2785bf1f3d564f7b
hasanm95 <hsncool10@gmail.com>
2026-08-03 10:07:02 +0600

    added udp server

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
     fmt.Println("Dingo server starting")
+    udpAddr, err := net.ResolveUDPAddr("udp", ":2053")
 
+    if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+    conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Dingo DNS server running on port 2053")
+
+    buf := make([]byte, 512)
+
+    for {
+        size, source, err := conn.ReadFromUDP(buf)
+        if err != nil {
+			fmt.Println("Error receiving data:", err)
+			break
+		}
+
+        receivedData := buf[:size]
+		fmt.Printf("Received %d bytes from %s\n", receivedData, source)
+
+
+		response := []byte("dummy response")
+
+        _, err = conn.WriteToUDP(response, source)
+		if err != nil {
+			fmt.Println("Failed to send response:", err)
+		}
+    }
 }
 
